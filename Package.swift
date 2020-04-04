ab1b4d86427e9a51de25d15fb5fce146858ed8c7
NSCabezon <nscabezon@gmail.com>
2020-04-04 19:56:21 +0200

    lowered to swift 5.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,7 @@ let package = Package(
 	dependencies: [
 		// Dependencies declare other packages that this package depends on.
 		.package(url: "git@github.com:Alamofire/Alamofire.git", from: "5.0.5"),
-		.package(url: "git@github.com:NSCabezon/HelpersSPM.git", from: "1.0.6")
+		.package(url: "git@github.com:NSCabezon/HelpersSPM.git", .branch("master-5.1"))
 	],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
